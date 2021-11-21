b3356b7133f0cb52d328b4217c2a280cb0f1be5a
Louisa Ge <57604666+package-l@users.noreply.github.com>
2021-11-21 01:22:24 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let myBubbleChart;
+
+// load data
+Promise.all([
+    d3.csv("data/tweet_emojis.csv"),
+    d3.csv("data/emojiWIP4.csv")
+]).then(function(data) {
+    initVisualizations(data);
+}).catch(function(err) {
+    console.log(err);
+})
+
+function initVisualizations(allDataArray) {
+    myBubbleChart = new EmojiBubble("emojibubble", allDataArray[0], allDataArray[1]);
+}
